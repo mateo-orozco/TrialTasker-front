6024efd55cb6412fc622d8851190b1dddec82e01
mateo-o <oromateo@hotmail.com>
2023-04-09 15:13:38 -0500

    formulario nuevo

--- src/views/CreateCasePRUEBAS.vue
+++ src/views/CreateCasePRUEBAS.vue
@@ -1,8 +1,8 @@
 <template>
     <HeaderAccions title="Crear Caso" to="CasosActivos" />
-    <Form :create="cases && cases.createCase" :form="form" button-text="Crear">
+    <!-- <Form :create="cases.createCase" :form="form" button-text="Crear">
         <FormGroup label="Nombre" :error="cases.errors ? cases.errors.case_name : []">
-            <input type="text" id="case_name" v-model="form.case_name">
+            <input type="text" id="case_name" :modelValue="form.case_name" >
         </FormGroup>
         <FormGroup label="Radicado" :error="cases.errors ? cases.errors.case_radicate : []">
             <input type="text" id="case_radicate" v-model="form.case_radicate">
@@ -20,7 +20,59 @@
                 <option :value="auth.user.id">{{ auth.user.name }} {{ auth.user.lastname }}</option>
             </select>
         </FormGroup>
-    </Form>
+    </Form> -->
+    <form @submit.prevent="cases.createCase(form)" class="form">
+     <div class="form-group">
+         <label for="case_name">Nombre</label>
+         <input type="text" id="case_name" v-model="form.case_name">
+         <div v-if="cases.errors" class="error">
+             <div v-for="error in cases.errors.case_name" :key="error">
+                 {{ error }}
+             </div>
+         </div>
+     </div>
+ 
+        <div class="form-group">
+            <label for="case_radicate">Radicado</label>
+            <input type="text" id="case_radicate" v-model="form.case_radicate">
+            <div v-if="cases.errors" class="error">
+                <div v-for="error in cases.errors.case_radicate" :key="error">
+                    {{ error }}
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="case_person_id">Persona</label>
+            <select id="case_person_id" v-model="form.case_person_id">
+                <option value="">Cliente</option>
+                <option v-for="person in persons.personsAll" :value="person.id">{{ person.per_name }} {{
+                    person.per_lastname }}</option>
+            </select>
+            <div v-if="cases.errors" class="error">
+                <div v-for="error in cases.errors.case_person_id" :key="error">
+                    {{ error }}
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="case_user_id">Abogado</label>
+            <select id="case_user_id" v-model="form.case_user_id">
+                <option value="">Seleccione un abogado</option>
+                <option :value="auth.user.id">{{ auth.user.name }} {{ auth.user.lastname }}</option>
+            </select>
+            <div v-if="cases.errors" class="error">
+                <div v-for="error in cases.errors.case_user_id" :key="error">
+                    {{ error }}
+                </div>
+            </div>
+        </div>
+        <button type="submit">Crear</button>
+        
+    </form>
+    <!-- <form @submit.prevent=" cases.createCase(form)">
+    </form>  -->
 </template>
 <script setup>
 import { onMounted, ref } from 'vue';
@@ -32,12 +84,12 @@ import { usePersonStore } from '@/stores/personStore';
 import { useAuthStore } from "@/stores/authStore";
 
 
-const cases = useCaseStore();
-const persons = usePersonStore();
-const auth = useAuthStore();
-console.log("--------cases--------")
-console.log(cases);
-const form = ref({
+    const cases = useCaseStore();
+    const persons = usePersonStore();
+    const auth = useAuthStore();
+    console.log("--------cases--------")
+    console.log(cases);
+    const form = ref({
     case_name: '',
     case_radicate: '',
     case_person_id: '',
@@ -45,7 +97,10 @@ const form = ref({
     case_status: true,
 });
 
+
+
 onMounted(() => {
+   
     persons.getPersonsAll();
     auth.getUser();
 });
